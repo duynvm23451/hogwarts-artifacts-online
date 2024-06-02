c7969e38b280c98069e2fe9c56bf0a9438131757
duynvm1711 <duynvm1711@gmail.com>
2024-06-02 21:17:33 +0700

    Add a github actions workflow to build and test every PR

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/security/SecurityConfiguration.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/security/SecurityConfiguration.java
@@ -64,6 +64,7 @@ public class SecurityConfiguration {
         return http
                 .authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest
                         .requestMatchers(HttpMethod.GET, this.baseUrl + "/artifacts/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, this.baseUrl + "/wizards/**").permitAll()
                         .requestMatchers(HttpMethod.GET, this.baseUrl + "/users/**").hasAnyAuthority("ROLE_admin")
                         .requestMatchers(HttpMethod.POST, this.baseUrl + "/users").hasAnyAuthority("ROLE_admin")
                         .requestMatchers(HttpMethod.PUT, this.baseUrl + "/users/**").hasAnyAuthority("ROLE_admin")
